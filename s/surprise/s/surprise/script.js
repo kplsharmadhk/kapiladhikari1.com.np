11e2ad532037a9039d78776aefa60e7d6d14d0c5
kplsharmadhk <kplsharmadhk@gmail.com>
2026-09-12 16:45:27 +0545

    Add script to update heading on DOMContentLoaded

---
+++ s/surprise/s/surprise/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const heading = document.querySelector("h1");
+
+    if (heading) {
+        heading.textContent = "Surprise Page Working ❤️";
+    }
+});
